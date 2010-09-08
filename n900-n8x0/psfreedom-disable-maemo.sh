f4f8c876aea52d1167d3ef7987734965e9792839
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2010-09-08 05:59:25 -0400

    Only force removal of the module if it's unused and has that -1 used bug

--- n900-n8x0/psfreedom-disable-maemo.sh
+++ n900-n8x0/psfreedom-disable-maemo.sh
@@ -3,7 +3,11 @@ RC=0
 
 if grep -q psfreedom /proc/modules; then
     logger "$0: removing psfreedom"
-    /sbin/rmmod -f psfreedom
+    if lsmod | grep psfreedom | grep -q 4294967295; then
+       /sbin/rmmod -f psfreedom
+    else
+      /sbin/rmmod psfreedom
+    fi
 fi
 
 /usr/sbin/osso-usb-mass-storage-enable.sh
