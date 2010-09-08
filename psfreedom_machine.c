503a5c762ac1db22c6d9829e3f9d029f33f44f06
DrMon <dr.mon_@hotmail.com>
2010-09-08 23:31:04 +1000

    Cleaned and removed unnecessary structs and code.

--- psfreedom_machine.c
+++ psfreedom_machine.c
@@ -137,123 +137,21 @@ static void psfreedom_set_address (struct usb_gadget *g, u8 address)
 */
 #define UI_GADGET_OFFSET 1724
 #define USBDEVADDR (readu((unsigned)g - UI_GADGET_OFFSET) + 0x0154)
-#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
-
 
 
 static inline void writel(unsigned long l, unsigned long addr)
 {
-	*(volatile unsigned long __force *)addr = l;
+  *(volatile unsigned long __force *)addr = l;
 }
 static inline unsigned long readl(unsigned long addr)
 {
-	return *(volatile unsigned long __force *)addr;
+  return *(volatile unsigned long __force *)addr;
 }
 static inline unsigned readu(unsigned addr)
 {
-	return *(volatile unsigned __force *)addr;
+  return *(volatile unsigned __force *)addr;
 }
-struct msm_request {
-	struct usb_request req;
 
-	/* saved copy of req.complete */
-	void	(*gadget_complete)(struct usb_ep *ep,
-					struct usb_request *req);
-
-
-	struct usb_info *ui;
-	struct msm_request *next;
-
-	unsigned busy:1;
-	unsigned live:1;
-	unsigned alloced:1;
-
-	dma_addr_t dma;
-	dma_addr_t item_dma;
-
-	struct ept_queue_item *item;
-};
-
-struct msm_endpoint {
-	struct usb_ep ep;
-	struct usb_info *ui;
-	struct msm_request *req; /* head of pending requests */
-	struct msm_request *last;
-	unsigned flags;
-
-	/* bit number (0-31) in various status registers
-	** as well as the index into the usb_info's array
-	** of all endpoints
-	*/
-	unsigned char bit;
-	unsigned char num;
-
-	/* pointers to DMA transfer list area */
-	/* these are allocated from the usb_info dma space */
-	struct ept_queue_head *head;
-};
-
-struct usb_info {
-	/* lock for register/queue/device state changes */
-	spinlock_t lock;
-
-	/* single request used for handling setup transactions */
-	struct usb_request *setup_req;
-
-	struct platform_device *pdev;
-	int irq;
-	void *addr;
-
-	unsigned state;
-	unsigned flags;
-
-	unsigned	online:1;
-	unsigned	running:1;
-
-	struct dma_pool *pool;
-
-	/* dma page to back the queue heads and items */
-	unsigned char *buf;
-	dma_addr_t dma;
-
-	struct ept_queue_head *head;
-
-	/* used for allocation */
-	unsigned next_item;
-	unsigned next_ifc_num;
-
-	/* endpoints are ordered based on their status bits,
-	** so they are OUT0, OUT1, ... OUT15, IN0, IN1, ... IN15
-	*/
-	struct msm_endpoint ept[32];
-
-	int *phy_init_seq;
-	void (*phy_reset)(void);
-	void (*hw_reset)(bool en);
-
-	/* for notification when USB is connected or disconnected */
-	void (*usb_connected)(int);
-
-	struct work_struct work;
-	unsigned phy_status;
-	unsigned phy_fail_count;
-
-	struct usb_gadget		gadget;
-	struct usb_gadget_driver	*driver;
-
-#define ep0out ept[0]
-#define ep0in  ept[16]
-
-	struct clk *clk;
-	struct clk *pclk;
-	struct clk *otgclk;
-	struct clk *ebi1clk;
-
-	unsigned int ep0_dir;
-	u16 test_mode;
-
-	u8 remote_wakeup;
-};
 static int psfreedom_is_high_speed (void)
 {
   return 1;
@@ -281,34 +179,29 @@ static char *psfreedom_get_endpoint_name (struct usb_endpoint_descriptor *desc)
 
 static u8 psfreedom_get_address (struct usb_gadget *g)
 {
-  struct usb_info *ui = container_of(g, struct usb_info, gadget);
   unsigned long buffer = 0;
   u8 address = 0;
 
-  if (ui) {
-    buffer = readl(USBDEVADDR);
-    /* The address is in the bits 25-32 */
-    address = (u8) (buffer >> 25) & 0x7F;
-    dev_vdbg(&g->dev, "***** Getting address : %d\n", address);
-  }
+  buffer = readl(USBDEVADDR);
+  /* The address is in the bits 25-32 */
+  address = (u8) (buffer >> 25) & 0x7F;
+  dev_vdbg(&g->dev, "***** Getting address : %d\n", address);
 
   return address;
 }
 
 static void psfreedom_set_address (struct usb_gadget *g, u8 address)
 {
-  struct usb_info *ui = container_of(g, struct usb_info, gadget);
 
-  if (ui) {
-    /* Send the address in bits 25-32. Do not use the same method as the
-       controller's SET_ADDRESS which sets bit 24 to '1' to tell the controller
-       to delay the operation until a IN response is sent (response to the
-       SET_ADDRESS must be sent with the old address).
-    */
-    writel((address << 25), USBDEVADDR);
-    dev_vdbg(&g->dev, "***** Setting address to %d. New address: %d\n",
-        address, psfreedom_get_address(g));
-  }
+  /* Send the address in bits 25-32. Do not use the same method as the
+     controller's SET_ADDRESS which sets bit 24 to '1' to tell the controller
+     to delay the operation until a IN response is sent (response to the
+     SET_ADDRESS must be sent with the old address).
+  */
+  writel((address << 25), USBDEVADDR);
+  dev_vdbg(&g->dev, "***** Setting address to %d. New address: %d\n",
+      address, psfreedom_get_address(g));
+  
 }
 
 #endif /* ENABLE_MSM72K_CONTROLLER */
