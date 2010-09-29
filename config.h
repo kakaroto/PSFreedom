219090be0593afec8a7a43a2181704b519935b8c
Youness Alaoui <kakaroto@kakaroto.homelinux.net>
2010-09-29 06:35:11 -0400

    use pl3's config.h since we depend on its settings too... (for now)

--- config.h
+++ config.h
@@ -12,12 +12,7 @@
 #define PSFREEDOM_CONFIG_H
 
 
-/* Define this macro if want to use the jig method */
-#define USE_JIG
-
-/* Define the firmware version you want to build PSFreedom for */
-#define FIRMWARE_3_41
-//#define FIRMWARE_3_01
+#include "pl3/config.h"
 
 #define DEBUG
 //#define VERBOSE_DEBUG
